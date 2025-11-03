f48f35ab13a37a046906e9064394735837f006f6
zohaib khalid <zohaibchaudry6@gmail.com>
2025-11-03 22:53:41 +0500

    adddd vercel code

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
  databaseconnection()
 app.use(cors({
-  origin: ["http://localhost:5173", "https://certiicationssfrontend.vercel.app/"],
+  origin: ["http://localhost:5173", "https://certiicationssfrontend.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     credentials:true
 }));

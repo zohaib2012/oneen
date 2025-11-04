c113f676600316d35175535a0585c2ab72c7118d
zohaib khalid <zohaibchaudry6@gmail.com>
2025-11-04 23:00:15 +0500

    sss

--- api/model/certificate.model.js
+++ api/model/certificate.model.js
@@ -2,8 +2,8 @@ import mongoose from "mongoose";
 
 const certificateSchema = new mongoose.Schema({
   certificateNumber: { type: String, required: true },
-  issueDate: { type: Date, required: true },
-  expiryDate: { type: Date, required: true },
+  issueDate: { type: String, required: true },
+  expiryDate: { type: String, required: true },
   customerImage: [String],
   customerName: { type: String, required: true },
   idNumber: { type: String, required: true },
@@ -11,7 +11,7 @@ const certificateSchema = new mongoose.Schema({
   nationality: { type: String, required: true },
   profession: { type: String, required: true },
   educationalProgram: { type: String, required: true },
-  programCompletionDate: { type: Date, required: true },
+  programCompletionDate: { type: String, required: true },
   establishmentName: { type: String, required: true },
   establishmentLicense: { type: String, required: true },
 }, { timestamps: true });
